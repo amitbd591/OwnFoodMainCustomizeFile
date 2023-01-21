791783063c5122500236f84da363f6ae215bc741
amitbd591 <122339814+amitbd591@users.noreply.github.com>
2023-01-21 16:49:21 +0600

    Add Update Code

--- src/Components/ChildComponents/Recipe_Gallery.jsx
+++ src/Components/ChildComponents/Recipe_Gallery.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Recipe_Gallery = () => {
   return (
     <section className='Recipe_Gallery section '>
-      <Container fluid>
+      <Container>
         {/* Top Gallery */}
         <div className='top__gallery'>
           <Row>
             <Col lg={6} className='leftSide'>
               <div className='leftSide_inner'>
                 <div className='header_text'>
-                  <h2>Down Home Ribs</h2>
+                  <h2>Indian Dishes</h2>
                 </div>
                 <div className='topBar'>
                   <Link to={"/ShoppingDetails"}>
@@ -26,7 +26,7 @@ const Recipe_Gallery = () => {
                       <div className='overlay__text'>
                         <div className='inner__body'>
                           <div>
-                            <h3>Indian Foods Overlay</h3>
+                            <h3>Butter Chicken</h3>
                             <p>
                               Lorem ipsum dolor, sit amet consectetur
                               adipisicing elit. Harum repellat dignissimos
@@ -39,26 +39,53 @@ const Recipe_Gallery = () => {
                   </Link>
                 </div>
                 <div className='buttonBar d-flex justify-content-between'>
-                  <div className='imgFile'>
-                    <img
-                      className='img-fluid'
-                      src={"/Assets/Img/Recipe/imgHub_1_2.png"}
-                      alt=''
-                    />
+                  <div className='imgFile fastImg'>
+                    <div>
+                      <img
+                        className='img-fluid'
+                        src={"/Assets/Img/Recipe/imgHub_1_2.png"}
+                        alt=''
+                      />
+                    </div>
+                    <div className='overlay__text'>
+                      <div className='inner__body'>
+                        <div>
+                          <h3>Chana Masala</h3>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className='imgFile'>
-                    <img
-                      className='img-fluid'
-                      src={"/Assets/Img/Recipe/imgHub_1_3.png"}
-                      alt=''
-                    />
+                  <div className='imgFile secondImg'>
+                    <div>
+                      <img
+                        className='img-fluid'
+                        src={"/Assets/Img/Recipe/imgHub_1_3.png"}
+                        alt=''
+                      />
+                    </div>
+                    <div className='overlay__text'>
+                      <div className='inner__body'>
+                        <div>
+                          <h3>Fish Curry</h3>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className='imgFile'>
-                    <img
-                      className='img-fluid'
-                      src={"/Assets/Img/Recipe/imgHub_1_4.png"}
-                      alt=''
-                    />
+                  <div className='imgFile thirdImg'>
+                    <div>
+                      <img
+                        className='img-fluid'
+                        src={"/Assets/Img/Recipe/imgHub_1_4.png"}
+                        alt=''
+                      />
+                    </div>
+                    <div className='overlay__text'>
+                      <div className='inner__body'>
+                        <div>
+                          <h3>Chicken Tikka Masala</h3>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -66,39 +93,80 @@ const Recipe_Gallery = () => {
             <Col lg={6} className='rightSide'>
               <div className='rightSide_inner'>
                 <div className='header_text'>
-                  <h2>Barbecued Drumsticks</h2>
+                  <h2>Japanese Dish</h2>
                 </div>
                 <div className='d-flex justify-content-between'>
-                  <div className='leftBar pe-4'>
+                  <div className='leftBar'>
                     <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_2_4.png"}
-                        alt=''
-                      />
+                      <div>
+                        <img
+                          className='img-fluidp'
+                          src={"/Assets/Img/Recipe/imgHub_2_4.png"}
+                          alt=''
+                        />
+                      </div>
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Ramen</h3>
+                            <p>
+                              Lorem ipsum dolor, sit amet consectetur
+                              adipisicing elit. Harum repellat dignissimos
+                              repellendus, esse iusto voluptas.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                   <div className='rightBar '>
-                    <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_2_2.png"}
-                        alt=''
-                      />
+                    <div className='imgFile firstImg'>
+                      <div>
+                        <img
+                          className='img-fluidp'
+                          src={"/Assets/Img/Recipe/imgHub_2_2.png"}
+                          alt=''
+                        />
+                      </div>
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Tempura</h3>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_2_3.png"}
-                        alt=''
-                      />
+                    <div className='imgFile secondImg'>
+                      <div>
+                        <img
+                          className='img-fluidp'
+                          src={"/Assets/Img/Recipe/imgHub_2_3.png"}
+                          alt=''
+                        />
+                      </div>
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Kushiyaki </h3>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_2_1.png"}
-                        alt=''
-                      />
+                    <div className='imgFile thirdImg'>
+                      <div>
+                        <img
+                          className='img-fluidp'
+                          src={"/Assets/Img/Recipe/imgHub_2_1.png"}
+                          alt=''
+                        />
+                      </div>
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Takoyaki</h3>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -112,46 +180,103 @@ const Recipe_Gallery = () => {
             <Col lg={6} className='leftSide'>
               <div className='leftSide__inner'>
                 <div className='header_text'>
-                  <h2>Classic Fried Chicken</h2>
+                  <h2>Thailand Foods</h2>
                 </div>
                 <div className='d-flex'>
-                  <div className='leftBar  pe-4'>
-                    <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_4_4.png"}
-                        alt=''
-                      />
+                  <div className='leftBar  pe-2'>
+                    <div className='imgFile  firstImg'>
+                      <div>
+                        <img
+                          className='img-fluid'
+                          src={"/Assets/Img/Recipe/imgHub_4_4.png"}
+                          alt=''
+                        />
+                      </div>
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Tom Yum Goong</h3>
+                            <p>
+                              Lorem ipsum dolor, sit amet consectetur
+                              adipisicing elit. Harum repellat dignissimos
+                              repellendus, esse iusto voluptas.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_4_5.png"}
-                        alt=''
-                      />
+                    <div className='imgFile  secondImg'>
+                      <div>
+                        <img
+                          className='img-fluid'
+                          src={"/Assets/Img/Recipe/imgHub_4_5.png"}
+                          alt=''
+                        />
+                      </div>
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Tom Kha Gai</h3>
+                            <p>
+                              Lorem ipsum dolor, sit amet consectetur
+                              adipisicing elit. Harum repellat dignissimos
+                              repellendus, esse iusto voluptas.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                   <div className='rightBar'>
-                    <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_4_1.png"}
-                        alt=''
-                      />
+                    <div className='imgFile firstImg'>
+                      <div>
+                        <div></div>
+                        <img
+                          className='img-fluid'
+                          src={"/Assets/Img/Recipe/imgHub_4_1.png"}
+                          alt=''
+                        />
+                      </div>
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Green Papaya Salad</h3>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_4_2.png"}
-                        alt=''
-                      />
+                    <div className='imgFile secondImg'>
+                      <div>
+                        <img
+                          className='img-fluid'
+                          src={"/Assets/Img/Recipe/imgHub_4_2.png"}
+                          alt=''
+                        />
+                      </div>
+
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Spicy Salad</h3>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div className='imgFile'>
-                      <img
-                        className='img-fluid'
-                        src={"/Assets/Img/Recipe/imgHub_4_3.png"}
-                        alt=''
-                      />
+                    <div className='imgFile thirdImg'>
+                      <div>
+                        <img
+                          className='img-fluid'
+                          src={"/Assets/Img/Recipe/imgHub_4_3.png"}
+                          alt=''
+                        />
+                      </div>
+                      <div className='overlay__text'>
+                        <div className='inner__body'>
+                          <div>
+                            <h3>Khao Pad</h3>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -163,42 +288,100 @@ const Recipe_Gallery = () => {
                   <h2>Gooey Country Ribs</h2>
                 </div>
                 <div className='topBar'>
-                  <div className='imgFile'>
-                    <img
-                      className='img-fluid'
-                      src={"/Assets/Img/Recipe/imgHub_3_4.png"}
-                      alt=''
-                    />
+                  <div className='imgFile firstImg'>
+                    <div>
+                      <img
+                        className='img-fluid '
+                        src={"/Assets/Img/Recipe/imgHub_3_4.png"}
+                        alt=''
+                      />
+                    </div>
+
+                    <div className='overlay__text'>
+                      <div className='inner__body'>
+                        <div>
+                          <h3>Tom Yum Goong</h3>
+                          <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Harum repellat dignissimos repellendus, esse
+                            iusto voluptas.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className='imgFile'>
-                    <img
-                      className='img-fluid'
-                      src={"/Assets/Img/Recipe/imgHub_3_5.png"}
-                      alt=''
-                    />
+                  <div className='imgFile secondImg'>
+                    <div>
+                      <img
+                        className='img-fluid'
+                        src={"/Assets/Img/Recipe/imgHub_3_5.png"}
+                        alt=''
+                      />
+                    </div>
+
+                    <div className='overlay__text'>
+                      <div className='inner__body'>
+                        <div>
+                          <h3>Tom Yum Goong</h3>
+                          <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Harum repellat dignissimos repellendus, esse
+                            iusto voluptas.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className='bottomBar '>
-                  <div className='imgFile'>
-                    <img
-                      className='img-fluid'
-                      src={"/Assets/Img/Recipe/imgHub_3_1.png"}
-                      alt=''
-                    />
+                  <div className='imgFile firstImg'>
+                    <div>
+                      <img
+                        className='img-fluid'
+                        src={"/Assets/Img/Recipe/imgHub_3_1.png"}
+                        alt=''
+                      />
+                    </div>
+                    <div className='overlay__text'>
+                      <div className='inner__body'>
+                        <div>
+                          <h3>Tom Yum Goong</h3>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className='imgFile'>
-                    <img
-                      className='img-fluid'
-                      src={"/Assets/Img/Recipe/imgHub_3_1.png"}
-                      alt=''
-                    />
+                  <div className='imgFile secondImg'>
+                    <div>
+                      <img
+                        className='img-fluid'
+                        src={"/Assets/Img/Recipe/imgHub_3_2.png"}
+                        alt=''
+                      />
+                    </div>
+
+                    <div className='overlay__text'>
+                      <div className='inner__body'>
+                        <div>
+                          <h3>Tom Yum Goong</h3>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className='imgFile'>
-                    <img
-                      className='img-fluid'
-                      src={"/Assets/Img/Recipe/imgHub_3_3.png"}
-                      alt=''
-                    />
+                  <div className='imgFile thirdImg'>
+                    <div>
+                      <img
+                        className='img-fluid'
+                        src={"/Assets/Img/Recipe/imgHub_3_3.png"}
+                        alt=''
+                      />
+                    </div>
+                    <div className='overlay__text'>
+                      <div className='inner__body'>
+                        <div>
+                          <h3>Tom Yum Goong</h3>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
